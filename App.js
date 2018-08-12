b9f36abbfba5774c6b5a2b9fb4bc6c871bc835f4
kyo88765 <kyo88765@gmail.com>
2018-08-12 12:05:51 +0900

    feat android-file_access

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import { Text, View, WebView } from 'react-native';
+import CustomWebView from "react-native-webview-android-file-upload";
+import { Text, View, Platform, WebView } from 'react-native';
 
 export default class App extends Component {
 
@@ -9,12 +10,22 @@ export default class App extends Component {
   }
 
   render() {
-    return (
-      <WebView
-        source={{ uri: "http://35.200.99.144:3000/view/signup" }}
-        style={{ marginTop: 20 }}
-        allowsInlineMediaPlayback={true}
-      />
-    );
+    if (Platform.OS === 'ios')
+      return (
+        <WebView
+          source={{ uri: "http://35.200.99.144:3000/view/signup" }}
+          style={{ marginTop: 20 }}
+          allowsInlineMediaPlayback={true}
+        />
+      );
+    else {
+      return (
+        <CustomWebView
+          source={{ uri: "http://35.200.99.144:3000/view/signup" }}
+          style={{ marginTop: 20 }}
+          allowsInlineMediaPlayback={true}
+        />
+      );
+    }
   }
 }
